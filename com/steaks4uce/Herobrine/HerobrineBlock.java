e3f8490c3dfdd40cd1df65aa4c48064d7f351c83
Krecklow, Nicholas R <owner@nkrecklow.com>
2011-11-27 01:10:56 -0600

    Added 2 new possible spawn messages and fixed some bugs.

--- com/steaks4uce/Herobrine/HerobrineBlock.java
+++ com/steaks4uce/Herobrine/HerobrineBlock.java
@@ -7,14 +7,14 @@
 /*    */ import org.bukkit.entity.Player;
 /*    */ import org.bukkit.event.block.BlockIgniteEvent;
 /*    */ import org.bukkit.event.block.BlockListener;
+         import java.util.Random;
 /*    */ 
 /*    */ public class HerobrineBlock extends BlockListener
 /*    */ {
 /*    */   public static Herobrine plugin;
 /*    */   public static int i;
 /*    */ 
-/*    */   public HerobrineBlock(Herobrine instance)
-/*    */   {
+/*    */   public HerobrineBlock(Herobrine instance) {
 /* 18 */     plugin = instance;
 /*    */   }
 /*    */ 
@@ -29,9 +29,7 @@
 /* 29 */       if ((netherRack.getType() == Material.NETHERRACK) && (mossyCobble.getType() == Material.MOSSY_COBBLESTONE) && (plugin.herobrineModel == null)) {
 /* 30 */         if (Herobrine.changeEnvironment.booleanValue() == true) {
 /* 31 */           w.setStorm(true);
-/* 32 */           while (w.getTime() != 14200L) {
-/* 33 */             w.setTime(w.getTime() + 1L);
-/*    */           }
+/* 32 */           w.setTime(14200); 
 /*    */         }
 /* 36 */         if (Herobrine.removeMossyCobblestone.booleanValue() == true) {
 /* 37 */           mossyCobble.setType(Material.COBBLESTONE);
@@ -41,7 +39,15 @@
 /* 41 */           w.createExplosion(b.getLocation(), -1.0F);
 /*    */         }
 /* 43 */         if (Herobrine.sendMessages.booleanValue() == true) {
-/* 44 */           plugin.getServer().broadcastMessage("<Herobrine> Only god can help you now!");
+                   Random messages = new Random();
+                   int message = messages.nextInt(3);
+                   if (message == 0) {
+/* 44 */               plugin.getServer().broadcastMessage("<Herobrine> Only god can help you now!");
+                   } else if (message == 1) {
+                       plugin.getServer().broadcastMessage("<Herobrine> Prepare for hell!");
+                   } else {
+                       plugin.getServer().broadcastMessage("<Herobrine> Never doubt my power!");
+                   }
 /*    */         }
 /* 46 */         Herobrine.trackingEntity = Boolean.valueOf(true);
 /* 47 */         w.spawnCreature(b.getLocation(), CreatureType.ZOMBIE);
